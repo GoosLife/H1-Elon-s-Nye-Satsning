a84f9406a80055591d8b052ffb591b60b48bd85d
GoosLife <72657308+GoosLife@users.noreply.github.com>
2022-08-29 14:02:25 +0200

    Add project files.

---
+++ H1 Elon's Nye Satsning/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace H1_Elon_s_Nye_Satsning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isRunning = true;
+
+            RemoteControlledCar car1 = new RemoteControlledCar(xPos: 0, yPos: 0);
+            RemoteControlledCar car2 = new RemoteControlledCar("Green", 30, 30);
+
+            List<RemoteControlledCar> cars = new List<RemoteControlledCar>();
+            cars.Add(car1);
+            cars.Add(car2);
+
+            while (isRunning)
+            {
+                // Draw cars and HUD on console
+                DrawCars(cars);
+                DrawCarHUD(cars);
+                
+                // Read and respond to input
+                ConsoleKey input = Console.ReadKey(true).Key;
+
+                switch (input)
+                {
+                    // Car1 controls
+                    case ConsoleKey.W:
+                        EraseTrack(car1);
+                        car1.Drive(Direction.NORTH);
+                        break;
+                    case ConsoleKey.A:
+                        EraseTrack(car1);
+                        car1.Drive(Direction.WEST);
+                        break;
+                    case ConsoleKey.S:
+                        EraseTrack(car1);
+                        car1.Drive(Direction.SOUTH);
+                        break;
+                    case ConsoleKey.D:
+                        EraseTrack(car1);
+                        car1.Drive(Direction.EAST);
+                        break;
+                    case ConsoleKey.Enter:
+                        car1.Charge();
+
+                    // Car2 controls
+                    case ConsoleKey.UpArrow:
+                        EraseTrack(car2);
+                        car2.Drive(Direction.NORTH);
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        EraseTrack(car2);
+                        car2.Drive(Direction.WEST);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        EraseTrack(car2);
+                        car2.Drive(Direction.SOUTH);
+                        break;
+                    case ConsoleKey.RightArrow:
+                        EraseTrack(car2);
+                        car2.Drive(Direction.EAST);
+                        break;
+
+                    // Break driving loop
+                    case ConsoleKey.Escape:
+                        isRunning = false;
+                        break;
+
+                    // Charge car1
+                        car2.Charge();
+                        break;
+
+                    // All other keys just continue running the loop.
+                    default:
+                        break;
+                }
+            }
+
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Erases the car from its current position before moving it to the new position
+        /// </summary>
+        static void EraseTrack(RemoteControlledCar c)
+        {
+            Console.SetCursorPosition(c.XPos, c.YPos);
+            Console.Write(' ');
+            Console.ResetColor();
+        }
+
+        /// <summary>
+        /// Move the cars around on the console.
+        /// </summary>
+        /// <param name="cars"></param>
+        static void DrawCars(List<RemoteControlledCar> cars)
+        {
+            foreach (RemoteControlledCar c in cars)
+            {
+                // Draw car at the cars current position
+                Console.SetCursorPosition(c.XPos, c.YPos);
+
+                // Convert object string with color value to System.ConsoleColor
+                Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), c.Color);
+
+                // Write an H representing the car.
+                Console.Write('H');
+
+                // Reset console color to default.
+                Console.ResetColor();
+            }
+        }
+
+        /// <summary>
+        /// Write current battery charge and metres driven since last charge to console.
+        /// </summary>
+        /// <param name="cars"></param>
+        static void DrawCarHUD(List<RemoteControlledCar> cars)
+        {
+            for (int i = 0; i < cars.Count; i++)
+            {
+                // Convert object string with color value to System.ConsoleColor
+                Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), cars[i].Color);
+
+                // Set cursor position to write details about the cars battery and currently driven distance in the side.
+                Console.SetCursorPosition(70, 10 * i);
+
+                // Write details to console.
+                Console.Write("Car " + i + " | Battery: " + cars[i].CurrentBattery + " | Metres since last charge: " + cars[i].DrivenMetres + "   ");
+            }
+        }
+    }
+}
---
+++ H1 Elon's Nye Satsning/RemoteControlledCar.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_Elon_s_Nye_Satsning
+{
+    public enum Direction
+    {
+        NORTH,
+        EAST,
+        SOUTH,
+        WEST
+    }
+    internal class RemoteControlledCar
+    {
+        private byte maxBatteryCapacity;
+        /// <summary>
+        /// Maximum capacity of the battery.
+        /// </summary>
+        private byte MaxBatteryCapacity
+        {
+            get { return maxBatteryCapacity; }
+            set { maxBatteryCapacity = value; }
+        }
+
+        private int metresPerBatteryPercentage;
+        /// <summary>
+        /// The number of metres a car can drive before it depletes 1% of battery.                                     
+        /// </summary>
+        private int MetresPerBatteryPercentage
+        {
+            get { return metresPerBatteryPercentage; }
+            set { metresPerBatteryPercentage = value; }
+        }
+
+        private byte currentBattery = 0;
+        /// <summary>
+        /// The cars current battery level.
+        /// </summary>
+        public byte CurrentBattery
+        {
+            get { return currentBattery; }
+            private set { currentBattery = value; }
+        }
+
+        private int drivenMetres = 0;
+        /// <summary>
+        /// The number of metres driven since the last charge.
+        /// </summary>
+        public int DrivenMetres
+        {
+            get { return drivenMetres; }
+            private set { drivenMetres = value; }
+        }
+
+        private string color;
+        /// <summary>
+        /// Color of the car.
+        /// </summary>
+        public string Color
+        {
+            get { return color; }
+            private set { color = value; }
+        }
+
+        private int xPos;
+        /// <summary>
+        /// The position of the car on the x axis.
+        /// </summary>
+        public int XPos
+        {
+            get { return xPos; }
+            private set { xPos = value; }
+        }
+
+        private int yPos;
+
+        public int YPos
+        {
+            get { return yPos; }
+            private set { yPos = value; }
+        }
+
+        public RemoteControlledCar(string color = "Red", int xPos = 0, int yPos = 0)
+        {
+            MaxBatteryCapacity = 100;
+            MetresPerBatteryPercentage = 20;
+            CurrentBattery = 100;
+            DrivenMetres = 0;
+            Color = color;
+            XPos = xPos;
+            YPos = yPos;
+        }
+
+        public void Drive(Direction direction)
+        {
+            int maxX = 60;
+            int maxY = 40;
+
+            if (CurrentBattery > 0)
+            {
+                switch(direction)
+                {
+                    case Direction.NORTH:
+                        if (YPos > 0)
+                            YPos--;
+                        break;
+                    case Direction.EAST:
+                        if (XPos < maxX)
+                            XPos++;
+                        break;
+                    case Direction.SOUTH:
+                        if (YPos < maxY)
+                            YPos++;
+                        break;
+                    case Direction.WEST:
+                        if (XPos > 0)
+                            XPos--;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException("Attempted to drive in non-existing direction.");
+                }
+
+                // Car has now driven 1 more metre
+                DrivenMetres++;
+
+                // Deplete battery every time the car has driven as many metres as defined in
+                // MetresPerBatteryPercentage
+                if (DrivenMetres > 0 &&
+                    DrivenMetres % MetresPerBatteryPercentage == 0)
+                { 
+                    DepleteBattery(); 
+                }
+            }
+        }
+
+        /// <summary>
+        /// Return car battery to full charge.
+        /// </summary>
+        public void Charge()
+        {
+            CurrentBattery = MaxBatteryCapacity;
+        }
+
+        public void DepleteBattery()
+        {
+            CurrentBattery--;
+        }
+    }
+}
